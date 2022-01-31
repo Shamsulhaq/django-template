6749daf20f35aafc7fc4efc3c42dc8c9900f851a
shamsul haq <bmshamsulhaq65@gmail.com>
2022-01-31 17:45:28 +0600

    add DRF setting

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
@@ -130,6 +130,27 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    # ),
+    # 'DEFAULT_FILTER_BACKENDS': (
+    #     'django_filters.rest_framework.DjangoFilterBackend',
+    # ),
+    # 'DEFAULT_PAGINATION_CLASS': (
+    #     'rest_framework.pagination.LimitOffsetPagination',
+    # ),
+    # 'PAGE_SIZE': (
+    #     20
+    # )
+
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
